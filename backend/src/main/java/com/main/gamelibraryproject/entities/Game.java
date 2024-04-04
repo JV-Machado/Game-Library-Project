37e360365c14b841ee787f81c19f30fd519d000b
Expedito Machado <joao.machado27@edu.unifor.br>
2024-04-04 02:10:41 -0300

    Add Game Validations

--- backend/src/main/java/com/main/gamelibraryproject/entities/Game.java
+++ backend/src/main/java/com/main/gamelibraryproject/entities/Game.java
@@ -1,5 +1,7 @@
 package com.main.gamelibraryproject.entities;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,19 +33,24 @@ public class Game {
 	private Long id;
 	
 	@Column(name="game_name")
+	@NotBlank
 	private String gameName;
 	
 	@Column(name="game_genre")
+	@NotBlank
 	private String gameGenre;
 	
 	@Column(name="release_year")
+	@NotNull
 	private Integer releaseYear;
 	
 	@Column(name="units_sold")
+	@NotNull
 	private Integer unitsSold;
 	
 	@ManyToOne
 	@JoinColumn(name="game_console_id")
+	@NotNull
 	private GameConsole gameConsole;
 	
 }
